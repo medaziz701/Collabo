095d82afc5ef689ab5d5b3c6f1c1e33361ca19fe
medaziz701 <zz.chaabani@gmail.com>
2026-07-31 12:59:08 +0200

    Rename adminRestController to AdminRestController to follow Java naming convention

--- pfe/src/main/java/com/CollaboraPro/pfe/RestController/AdminRestController.java
+++ pfe/src/main/java/com/CollaboraPro/pfe/RestController/AdminRestController.java
@@ -19,7 +19,7 @@ import java.util.Optional;
 @RestController
 @CrossOrigin("*")
 @RequestMapping(value = "/admin")
-public class adminRestController {
+public class AdminRestController {
     private BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
 
     @Autowired
