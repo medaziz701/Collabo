22a11f5a0fe4925529f8a2a5ac199140aaee1fde
medaziz701 <zz.chaabani@gmail.com>
2026-08-07 09:47:23 +0200

    Fix highlight.js CSS import: move from app.module.ts to styles.css

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HighlightModule } from 'ngx-highlightjs';
-import 'highlight.js/styles/github-dark.css';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
